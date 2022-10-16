23896f440e0543496f23428e7687edf060f11a95
Santo Khan <santokhan1999@gmail.com>
2022-10-16 22:04:18 +0600

    Create array_to_csv.js

---
+++ array_to_csv.js
@@ -0,0 +1,17 @@
+function array_to_csv(json) {
+  const array = json.slice();
+  let rows = array[0].join(",") + "\n";
+  array.shift();
+  array.forEach((e) => {
+    rows += e.join(",") + "\n";
+  });
+  return rows;
+}
+
+console.log(
+  array_to_csv([
+    ["keyword", "vol", "cpc"],
+    ["data", "1500000", "1.75"],
+    ["data structure", "1400000", "1.55"],
+  ])
+);
